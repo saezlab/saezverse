f7993fa7255e196960b39319afef913ad071282d
Türei Dénes <turei.denes@gmail.com>
2021-05-26 16:54:43 +0200

    R packages outside of Bioc

--- setup/install.R
+++ setup/install.R
@@ -12,3 +12,14 @@ BiocManager::install(c(
   "OmnipathR",
   "progeny"
 ), ask = FALSE)
+
+install.packages(
+  'https://github.com/saezlab/PHONEMeS/raw/master/Package/PHONEMeS_0.2.7.tar.gz',
+  repos = NULL
+)
+
+require(devtools)
+devtools::install_github('saezlab/decoupleR')
+devtools::install_github('saezlab/ocean')
+BiocManager::install('BioNet', ask = FALSE)
+devtools::install_github('saezlab/PHONEMeS-ILP')
