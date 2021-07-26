08f4c1bbf255203d5831d8c37d30cd1e146b496c
Jovan Tanevski <tanevski@gmail.com>
2021-07-26 15:51:00 +0200

    update packages available from pip

--- setup/dependencies.sh
+++ setup/dependencies.sh
@@ -6,7 +6,7 @@ apt -y install libcurl4-openssl-dev python3-dev
 apt -y install python3-igraph libcairo2-dev pkg-config
 
 # Python tools dependencies (unstated dependencies?)
-pip3 install --user --no-warn-script-location numpy scipy statsmodels pycurl pycairo
+pip3 install --user --no-warn-script-location bioservices numpy scipy statsmodels pycurl pycairo
 
 # PHONEMeS dependencies
 Rscript --vanilla -e 'BiocManager::install(c("BioNet", "tidyverse", "qvalue"), ask = FALSE)'
--- setup/installpy.sh
+++ setup/installpy.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 # Saez Python tools in PyPI
-pip3 install --user --no-warn-script-location pypath-omnipath omnipath bioservices
+pip3 install --user --no-warn-script-location pypath-omnipath omnipath progeny dorothea
 # Saez Python tools from git
-pip3 install --user --no-warn-script-location git+https://github.com/saezlab/progeny-py.git \
-	git+https://github.com/saezlab/dorothea-py.git \
-	git+https://github.com/saezlab/kinact.git
+pip3 install --user --no-warn-script-location git+https://github.com/saezlab/kinact.git
