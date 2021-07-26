a0031c711fa9275da89bdf467317065dc084f18b
Jovan Tanevski <tanevski@gmail.com>
2021-07-26 16:17:13 +0200

    add coinor-cbc to dependencies

--- setup/dependencies.sh
+++ setup/dependencies.sh
@@ -5,8 +5,10 @@ apt -y install libcurl4-openssl-dev python3-dev
 # Extras for pypath
 apt -y install python3-igraph libcairo2-dev pkg-config
 
+
 # Python tools dependencies (unstated dependencies?)
 pip3 install --user --no-warn-script-location bioservices numpy scipy statsmodels pycurl pycairo
 
 # PHONEMeS dependencies
+apt -y install coinor-cbc
 Rscript --vanilla -e 'BiocManager::install(c("BioNet", "tidyverse", "qvalue"), ask = FALSE)'
