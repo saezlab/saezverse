457f2a48b518f2e7758a00375e9e5181d18e2949
deeenes <turei.denes@gmail.com>
2024-12-03 11:52:45 +0100

    insall pip packages to `.saez` venv, activate it by default on login

--- setup/dependencies.sh
+++ setup/dependencies.sh
@@ -3,14 +3,18 @@
 # For building C-Python bindings
 apt -y install libcurl4-openssl-dev python3-dev
 # Extras for pypath
-apt -y install python3-igraph libcairo2-dev pkg-config
+apt -y install python3-igraph libcairo2-dev pkg-config python3-pip
 
 CRAN='options(repos = c(CRAN = "https://cloud.r-project.org"))'
 
 echo "$CRAN" >> ~/.Rprofile
 
+python -m venv ~/.saez
+echo 'source ~/.saez/bin/activate' >> ~/.bashrc
+source ~/.saez/bin/activate
+
 # Python tools dependencies (unstated dependencies?)
-pip3 install --user --no-warn-script-location bioservices numpy scipy statsmodels pycurl pycairo
+pip install --no-warn-script-location bioservices numpy scipy statsmodels pycurl pycairo
 
 # PHONEMeS dependencies
 apt -y install coinor-cbc
--- setup/installpy.sh
+++ setup/installpy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+source ~/.saez/bin/activate
 # Saez Python tools in PyPI
 pip3 install --user --no-warn-script-location pypath-omnipath omnipath decoupler
 # Saez Python tools from git
