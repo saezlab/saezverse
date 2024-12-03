d7d443c735b6c81a1ff904034011781e3adb046b
deeenes <turei.denes@gmail.com>
2024-12-03 11:53:04 +0100

    Python packages: added `networkcommons` and `corneto`

--- setup/installpy.sh
+++ setup/installpy.sh
@@ -2,6 +2,6 @@
 
 source ~/.saez/bin/activate
 # Saez Python tools in PyPI
-pip3 install --user --no-warn-script-location pypath-omnipath omnipath decoupler
+pip install --no-warn-script-location pypath-omnipath omnipath decoupler liana[common] networkcommons corneto
 # Saez Python tools from git
-pip3 install --user --no-warn-script-location git+https://github.com/saezlab/kinact.git
+pip install --no-warn-script-location git+https://github.com/saezlab/kinact.git
